3e038fe002545121c3df867b0b056ca61d6e106e
joliotg-sketch <joliotg@gmail.com>
2026-06-13 20:54:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'metres-v2';
+const CACHE = 'metres-v5';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
